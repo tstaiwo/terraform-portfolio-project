ab1f065c82f68bfc8e81a5def2b15c8ff1ff238f
Taiwo Taiwo <taiwostaiwo@yahoo.com>
2024-07-21 08:00:20 -0400

    t

---
+++ main.tf
@@ -0,0 +1,62 @@
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+
+#S3 Bucket
+resource "aws_s3_bucket" "nextjs_bucket" {
+  bucket = "nextjs-portfolio-bucket-tstaiwo"
+
+}
+
+
+#Ownership control
+resource "aws_s3_bucket_ownership_controls" "nextjs_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.nextjs_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPrefferred"
+  }
+}
+
+
+#Block Public Access
+resource "aws_s3_bucket_public_access_block" "nextjs_bucket_public_access_block" {
+  bucket                  = aws_s3_bucket.nextjs_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
+}
+
+
+#Bucket ACL
+resource "aws_s3_bucket_acl" "nextjs_bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.nextjs,
+    aws_s3_bucket_public_access_block.nextjs_bucket_public_access_block
+  ]
+  bucket = aws_s3_bucket.nextjs_bucket.id
+  acl    = "public-read"
+}
+
+
+#Bucket Policy
+resource "aws_s3_bucket_policy" "nextjs_bucket_policy" {
+  bucket = aws_s3_bucket.nextjs_bucket.id
+
+  policy = jsondecode(({
+    version = "2012-10-17"
+    statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.nextjs_bucket.arn}/*"
+      }
+    ]
+  }))
+
+}
